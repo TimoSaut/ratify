45708911d4db36e5a5ae7a4a77f535b6f42fe5c2
timosauter <sautertimo@t-online.de>
2025-09-25 16:12:16 +0200

    feat(screens): add skeleton screens

---
+++ lib/screens/album_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AlbumDetailScreen extends StatelessWidget {
+  const AlbumDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Album Detail Screen")),
+    );
+  }
+}
---
+++ lib/screens/song_detail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SongDetailScreen extends StatelessWidget {
+  const SongDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Song Detail Screen")),
+    );
+  }
+}
